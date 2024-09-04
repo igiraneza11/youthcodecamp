b21cd5364494e677a4db7264e9eb655fcda78fbb
Shyaka Chaste <liochastej@gmail.com>
2024-09-04 05:27:21 -0700

    I added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import '../App.css';
+import logo from '../assets/Logo.png';
+
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="Youth Code Camp Logo" />
+      </div>
+      <div className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#events">Events</a>
+        <a href="#community">Community</a>
+        <a href="#donate">Donate</a>
+        <a href="#projects">Projects</a>
+        <button className="learn-button">Learn</button>
+      </div>
+      <div className="menu-icon" onClick={toggleMobileMenu}>
+        <i className={isMobileMenuOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
